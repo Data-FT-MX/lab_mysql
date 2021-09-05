2837304f7e1a74fdb0428dfdc0f6e71ed1705590
Mariangel <angelsmreyes@gmail.com>
2021-09-05 10:08:28 -0400

    Update

--- your-code/create.sql
+++ your-code/create.sql
@@ -2,12 +2,13 @@ USE lab_mysql;
 
 CREATE DATABASE lab_mysql;
 
+
 CREATE TABLE lab_mysql.cars (
 id_car CHAR(5), VIN VARCHAR(45), man_facturer VARCHAR(45), 
 model VARCHAR(45), year_cars CHAR(4), color VARCHAR(45)
 );
 
-DROP TABLE cars;
+
 
 
 CREATE TABLE lab_mysql.customers (
@@ -16,20 +17,19 @@ email VARCHAR(45), addres VARCHAR(45), city VARCHAR(45), state_providence VARCHA
 country VARCHAR(45), zip_code VARCHAR(45)
 );
 
-DROP TABLE customers;
+
 
 CREATE TABLE lab_mysql.salesperson (
 id_sales CHAR(5), staff_id VARCHAR(45), name_sales VARCHAR(45), store VARCHAR(45)
 );
 
-DROP TABLE salesperson;
 
 CREATE TABLE lab_mysql.invoices (
 id_inv CHAR(5), ivc_number VARCHAR(45), date_ivc VARCHAR(45), car VARCHAR(45), customer VARCHAR(45),
 salesperson VARCHAR(45)
 );
 
-DROP TABLE invoices;
+
 
 /* SHOW DATABASES; */
  /* DROP DATABASE labmysql; */ 
